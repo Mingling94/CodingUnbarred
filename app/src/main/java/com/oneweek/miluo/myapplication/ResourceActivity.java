be84824ee33f38b727acc4d7812e997604132642
aechalf <aechalf@cs.washington.edu>
2016-07-26 12:13:53 -0700

    beginnings of web view stuff for resources activity

---
+++ app/src/main/java/com/oneweek/miluo/myapplication/ResourceActivity.java
@@ -0,0 +1,49 @@
+package com.oneweek.miluo.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.net.NetworkInfo;
+import android.net.ConnectivityManager;
+import android.webkit.WebViewClient;
+
+public class ResourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+
+        WebView webview;
+        webview = (WebView) findViewById(R.id.wv);
+        webview.setWebViewClient(new WebC());
+
+        // Enable Javascript
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webview.loadUrl("http://devdocs.io/offline");
+
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public class WebC extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+    }
+}
