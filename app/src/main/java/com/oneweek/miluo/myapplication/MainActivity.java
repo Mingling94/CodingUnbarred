93a727917539073a6fc9dce76a6a376c06b5acd4
aechalf <aechalf@cs.washington.edu>
2016-07-26 12:33:47 -0700

    beginnings of web view stuff for resources activity

--- app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
+++ app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.oneweek.miluo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+
+import android.view.View;
 import android.view.View;
 import android.content.Intent;
 
@@ -11,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.button);
+        assert button != null;
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent i = new Intent(MainActivity.this, DevModeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     public void goToResources(View view) {
