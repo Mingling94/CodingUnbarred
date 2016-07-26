1099dac4f7642097986f556438e9999c8c7dc5d8
Ming Luo (OWA) <miluo@microsoft.com>
2016-07-25 22:56:41 -0700

    Three other activities to navigate to

--- app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
+++ app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.oneweek.miluo.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
@@ -14,33 +14,27 @@ import android.content.Intent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button) findViewById(R.id.button);
-        assert button != null;
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                Intent i = new Intent(MainActivity.this, DevModeActivity.class);
-                startActivity(i);
-            }
-        });
-        final Button button = (Button) findViewById(R.id.button);
-        assert button != null;
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                Intent i = new Intent(MainActivity.this, DevModeActivity.class);
-                startActivity(i);
-            }
-        });
     }
 
-    public void goToResources(View view) {
-        Intent intent = new Intent(this, ResourceActivity.class);
+    // Courses button
+    public void showCourseSelection(View view) {
+        Intent intent = new Intent(this, CourseSelectionActivity.class);
+        startActivity(intent);
+    }
+
+    // Resources button
+    public void showResourceSelection(View view) {
+        Intent intent = new Intent(this, ResourceSelectionActivity.class);
+        startActivity(intent);
+    }
+
+    // Sandbox button
+    public void showSandbox(View view) {
+        Intent intent = new Intent(this, DeveloperModeActivity.class);
         startActivity(intent);
     }
 }
