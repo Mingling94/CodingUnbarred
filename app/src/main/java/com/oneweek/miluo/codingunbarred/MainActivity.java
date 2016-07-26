40eb38a5f3fa20b4a6ece22e7de7675292d668ac
Erin Peach <t-erpea@microsoft.com>
2016-07-26 11:18:01 -0700

    Got the icons to workgit add .!

--- app/src/main/java/com/oneweek/miluo/codingunbarred/MainActivity.java
+++ app/src/main/java/com/oneweek/miluo/codingunbarred/MainActivity.java
@@ -2,14 +2,29 @@ package com.oneweek.miluo.codingunbarred;
 
 import android.content.Intent;
 import android.os.Bundle;
+<<<<<<< b4795f1377a2673c9c5236ed9a805614f646bf82:app/src/main/java/com/oneweek/miluo/codingunbarred/MainActivity.java
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+=======
+import android.text.Html;
+import android.widget.Button;
+import android.widget.TextView;
+>>>>>>> Got the icons to workgit add .!:app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
+
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.EntypoModule;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 public class MainActivity extends AppCompatActivity {
+<<<<<<< b4795f1377a2673c9c5236ed9a805614f646bf82:app/src/main/java/com/oneweek/miluo/codingunbarred/MainActivity.java
     @Override
+=======
+
+>>>>>>> Got the icons to workgit add .!:app/src/main/java/com/oneweek/miluo/myapplication/MainActivity.java
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Iconify.with(new FontAwesomeModule());
 
         TextView tv1=(TextView)findViewById(R.id.textView);
 
@@ -23,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         b1.setTypeface(buttonFace);
         b2.setTypeface(buttonFace);
         b3.setTypeface(buttonFace);
+
     }
 
     // Courses button
