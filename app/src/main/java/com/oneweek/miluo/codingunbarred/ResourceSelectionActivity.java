d32cd2d8e34b2ed3be4f1d1d50e26ec8c930fbee
Jake Martin <jakevmartin@gmail.com>
2016-07-27 14:13:50 -0700

    Added resources cheat sheet

--- app/src/main/java/com/oneweek/miluo/codingunbarred/ResourceSelectionActivity.java
+++ app/src/main/java/com/oneweek/miluo/codingunbarred/ResourceSelectionActivity.java
@@ -2,6 +2,7 @@ package com.oneweek.miluo.codingunbarred;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ResourceSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ResourceSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resource_selection);
+        WebView resourceBox = (WebView)findViewById(R.id.resourceBox);
+        resourceBox.getSettings().setJavaScriptEnabled(true);
+        resourceBox.loadUrl("file:///android_asset/resources/HTML_Cheat_Sheet.html");
     }
 }
